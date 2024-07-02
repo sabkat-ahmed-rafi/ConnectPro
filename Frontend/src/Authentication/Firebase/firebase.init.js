bd75dcc2189b65379d120c7797db1261bf103af2
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-07-02 12:11:48 +0600

    firebase keys added

---
+++ Frontend/src/Authentication/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARZTJg3uqrxRUhYMVK4Ulh_AeGwPZj-FU",
+  authDomain: "connectpro-eb32a.firebaseapp.com",
+  projectId: "connectpro-eb32a",
+  storageBucket: "connectpro-eb32a.appspot.com",
+  messagingSenderId: "476853660126",
+  appId: "1:476853660126:web:59ffec868abe743eb1554f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
