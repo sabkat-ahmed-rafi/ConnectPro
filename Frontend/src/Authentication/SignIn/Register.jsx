d337e57c130f0396620dd86c118b8cf725b77267
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-07-06 19:10:42 +0600

    signup form's functionality added

--- Frontend/src/Authentication/SignIn/Register.jsx
+++ Frontend/src/Authentication/SignIn/Register.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import Lottie from "lottie-react";
 import SignIn from "../../../public/signIn.json"
 import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form"
+
 
 
 const Register = () => {
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+
+  const onSubmit = (data) => console.log(data)
 
 
     return (
@@ -13,34 +23,37 @@ const Register = () => {
           <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
-    <Lottie animationData={SignIn} loop={true} />
+    <Lottie className='lg:flex hidden' animationData={SignIn} loop={true} />
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
+      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
         <h1 className='text-3xl  font-bold text-center pb-2'>Create an account</h1>
         <div className="form-control">
           <label className="label">
             <span className="label-text font-semibold">Name</span>
           </label>
-          <input type="text" placeholder="Name" className="input input-bordered" required />
+          <input {...register("name", { required: true })} type="text" placeholder="Name" className="input input-bordered" />
+          {errors.name && <span className='text-red-600 pt-2'>Name is required</span>}
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text font-semibold">Email</span>
           </label>
-          <input type="email" placeholder="Email" className="input input-bordered" required />
+          <input {...register("email", { required: true })} type="email" placeholder="Email" className="input input-bordered" />
+          {errors.email && <span className='text-red-600 pt-2'>Email is required</span>}
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text font-semibold">Password</span>
           </label>
-          <input type="password" placeholder="Password" className="input input-bordered" required />
+          <input {...register("password", { required: true })} type="password" placeholder="Password" className="input input-bordered" />
+          {errors.password && <span className='text-red-600 pt-2'>Password is required</span>}
         </div>
         <div className="form-control mt-6">
-          <button className="font-semibold rounded px-5 py-1.5 overflow-hidden group bg-blue-500 relative hover:bg-gradient-to-r hover:from-sky-500 hover:to-indigo-400 text-white hover:ring-indigo-600 transition-all ease-out duration-300">Sign up</button>
+          <button className="font-semibold rounded px-5 py-1.5 overflow-hidden group bg-sky-500 relative hover:bg-gradient-to-r hover:from-sky-500 hover:to-indigo-400 text-white hover:ring-indigo-600 transition-all ease-out duration-300">Sign up</button>
         </div>
         <label className="label">
-            <p className="text-sm italic">Already have an account? <Link className='text-blue-600' to='/login'>Login</Link></p>
+            <p className="text-sm italic text-slate-500">Already have an account? <Link className='text-blue-600' to='/login'>Login</Link></p>
           </label>
       </form>
     </div>
