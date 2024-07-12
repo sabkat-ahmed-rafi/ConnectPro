c8ce136df6f6a931206b092023a9e1dcb373bdfd
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-07-13 03:59:41 +0600

    socket.io is integrated

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,21 @@ const cors = require('cors');
 const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
+const http = require('http');
+const { Server } = require('socket.io');
+
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "HEAD"],
+    credentials: true
+  }
+})
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -17,6 +32,8 @@ app.use(cors({
   optionSuccessStatus: 200
   })
 );
+
+
 
 
 const verifyToken = (req, res, next) => {
@@ -48,7 +65,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    // next time i have to add the verifyToken and axiosSecure to logout user after session expired 
+    const database = client.db("ConnectPro");
+    const usersCollection = database.collection("users"); 
     
     // Creating token and saving it to the cookies in browser 
     app.post("/jwt" , async (req, res) => {
@@ -81,6 +99,33 @@ async function run() {
 
 
 
+    // Socket.io events 
+    io.on("connection", (socket) => {
+      console.log(`socket connected: ${socket.id}`)
+
+      socket.on("register", async ({uid}) => {
+        console.log(`register user ${uid} with socket id: ${socket.id}`)
+        await usersCollection.updateOne(
+          {uid: uid},
+          { $set: {socketId: socket.id} },
+          { upsert: true }
+        )
+      })
+    
+      socket.on("chat message", (msg) => {
+        console.log(`message from ${socket.id} : ${msg}`)
+        io.emit("chat message", msg)
+      })
+    
+      socket.on("disconnect", () => {
+        console.log(`socket disconnected: ${socket.id}`)
+      })
+    
+    })
+    
+
+
+
 
     // await client.connect();
     // Send a ping to confirm a successful connection
@@ -99,6 +144,12 @@ app.get('/', (req, res) => {
     res.send('Hello Welcome to ConnectPro server.');
 })
 
-app.listen(port, () => {
+
+
+
+
+
+server.listen(port, () => {
     console.log(`ConnectPro server is running on port ${port}`);
 })
+
