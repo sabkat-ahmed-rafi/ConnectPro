891c987e056c92ce8cffed4b0f9a5af7e25f3d47
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-08-25 14:16:55 +0600

    Bug: video call time duration problem

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../Hooks/useAuth';
 
 const Home = () => {
 
   const {socket, setCallerInfo, setShowOutlet} = useAuth()
-
   const navigate = useNavigate()
 
       // Function to handle incoming call click and show IncomingCall
+      
       const handleIncomingCall = () => {
         navigate("/inbox/incomingCall");
+
+        setTimeout(() => {
+                navigate("/")
+        }, 5000);
     }
 
     useEffect(() => {
