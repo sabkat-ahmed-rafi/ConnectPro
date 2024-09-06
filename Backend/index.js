fbbe55b43e746545b9a76d8d8075edd1e4e2ff6b
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-09-06 20:50:00 +0600

    Update: environment variable changed

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [`${process.env.FRONTEND_URL}`],
     methods: ["GET", "HEAD"],
     credentials: true
   }
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: [`${process.env.FRONTEND_URL}`],
   credentials: true,
   optionSuccessStatus: 200
   })
